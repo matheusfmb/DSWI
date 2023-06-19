15b5041df2c06ef48c90bfd033824d06e1c647a2
Matheus Felipe <matheusbarros9107@gmail.com>
2023-06-19 19:43:15 -0300

    Página serviços e Sobre
    página de serviços e Sobre

--- avaliacao/js/script.js
+++ avaliacao/js/script.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
-    
     // home - conteudo
     gsap.to(".img-home", {duration: 2, x: 180});
     gsap.to(".text-content-home", {duration: 2, x: -130});
