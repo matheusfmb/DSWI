343ec829053bdb8e49b51e8e68fff2e297ea56cb
Matheus Felipe <matheusbarros9107@gmail.com>
2023-06-20 10:37:57 -0300

    validação básica form

--- avaliacao/js/script.js
+++ avaliacao/js/script.js
@@ -2,10 +2,30 @@ document.addEventListener("DOMContentLoaded", function() {
     // home - conteudo
     gsap.to(".img-home", {duration: 2, x: 180});
     gsap.to(".text-content-home", {duration: 2, x: -130});
-
-    // página de Agendamento E FORM CONTATO 
     gsap.to(".form-group", { opacity: 1, duration: 1.5 });
 
+    
+
+    document.getElementById('btn-contato').addEventListener('click', function(event) {
+        event.preventDefault();
+        const name = document.getElementById("name").value;
+        const phone = document.getElementById("phone").value;
+        const email = document.getElementById("email").value;
+        const notes = document.getElementById("notes").value;
+        if (name == "" && phone == "" && email == "" && notes == "") {
+            alert("Preencha todos os Campos");
+        }else{
+            alert("Contato enviado!");
+            document.getElementById("name").value = "";
+            document.getElementById("phone").value = "";
+            document.getElementById("email").value = "";
+            document.getElementById("notes").value = "";
+        }
+    });
+})
+
+
+document.addEventListener("DOMContentLoaded", function() {
     document.getElementById('btn-agendamento').addEventListener('click', function(event) {
         event.preventDefault();
         const name = document.getElementById("name").value;
@@ -23,24 +43,6 @@ document.addEventListener("DOMContentLoaded", function() {
             document.getElementById("email").value = "";
             document.getElementById("date").value = "";
             document.getElementById("time").value = "";
-            document.getElementById("notes").value = "";
-        }
-    });
-
-    document.getElementById('btn-contato').addEventListener('click', function(event) {
-        event.preventDefault();
-        const name = document.getElementById("name").value;
-        const phone = document.getElementById("phone").value;
-        const email = document.getElementById("email").value;
-    
-        const notes = document.getElementById("notes").value;
-        if (name == "" && phone == "" && email == "" && notes == "") {
-            alert("Preencha todos os Campos");
-        }else{
-            alert("Contato enviado!");
-            document.getElementById("name").value = "";
-            document.getElementById("phone").value = "";
-            document.getElementById("email").value = "";
             document.getElementById("notes").value = "";
         }
     });
